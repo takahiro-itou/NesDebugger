533abfae5fd24e1ae321c8333534394749af8a32
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-02-21 16:38:18 +0900

    メインビューを仮実装

--- ViewVb/MainView.vb
+++ ViewVb/MainView.vb
@@ -1,10 +1,30 @@
 ﻿Public Class MainView
 
+Private m_manNes As New NesDbgWrap.NesMan.NesManager
+Private m_manPpu As New NesDbgWrap.NesMan.BasePpuCore(Me.m_manNes)
+
+Private Function initializeScreen(
+        ByVal w As Integer, ByVal h As Integer) As Boolean
+''--------------------------------------------------------------------
+''    画面を初期化する
+''--------------------------------------------------------------------
+    Me.wfcGameView.initializeScreenImage(w, h)
+    Return True
+End Function
+
 Private Sub showGraphics()
 ''--------------------------------------------------------------------
 ''    画像を表示する
 ''--------------------------------------------------------------------
+    Me.wfcGameView.drawGraphics()
+End Sub
 
+Private Sub MainView_Load(sender As Object, e As EventArgs) Handles _
+            MyBase.Load
+''--------------------------------------------------------------------
+''    フォームのロードイベントハンドラ
+''--------------------------------------------------------------------
+    initializeScreen(256, 240)
 End Sub
 
 Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles _
