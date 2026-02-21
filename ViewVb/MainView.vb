a58a0439e4c50a0259de7a7b9d43246b84b113e0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-02-21 13:52:23 +0900

    不要になったサンプルコードを削除

--- ViewVb/MainView.vb
+++ ViewVb/MainView.vb
@@ -4,7 +4,7 @@ Private Sub showGraphics()
 ''--------------------------------------------------------------------
 ''    画像を表示する
 ''--------------------------------------------------------------------
-    SampleControl1.drawGraphics()
+
 End Sub
 
 Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles _
@@ -19,17 +19,6 @@ Private Sub mnuRunCount_Click(sender As Object, e As EventArgs) Handles _
             mnuRunCount.Click
 ''--------------------------------------------------------------------
 ''    メニュー「実行」－「カウント」
-''--------------------------------------------------------------------
-    System.Threading.Thread.Sleep(100)
-    showGraphics()
-End Sub
-
-Private Sub SampleControl1_RunButtonClick(sender As Object, e As EventArgs) _
-    Handles SampleControl1.RunButtonClick
-''--------------------------------------------------------------------
-''    イベントハンドラ
-''
-''    ユーザーコントロールの「実行ボタンクリック」
 ''--------------------------------------------------------------------
     System.Threading.Thread.Sleep(100)
     showGraphics()
