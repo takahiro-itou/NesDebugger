68f37fe61f0f1f5126eeb8bd8c6d4381ee885fee
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-02-22 09:35:25 +0900

    不具合修正

--- WinFormsControl/GameScreen.cs
+++ WinFormsControl/GameScreen.cs
@@ -55,6 +55,8 @@ public partial class GameScreen : UserControl
         System.Drawing.Brush    brushBG;
         System.Drawing.Color    colorBG;
 
+        this.m_wManPpu.drawScreen()
+
         imgCanvas = new System.Drawing.Bitmap(picView.Width, picView.Height);
         grpCanvas = System.Drawing.Graphics.FromImage(imgCanvas);
 
