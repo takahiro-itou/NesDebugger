ab9b26dcb027ec76cde339436e87806a12acba54
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-02-27 21:30:56 +0900

    空のラッパークラスを新規作成

---
+++ Wrapper/Common/ReferenceWrapper.h
@@ -0,0 +1,125 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of ReferenceWrapper class.
+**
+**      @file       NesMan/ReferenceWrapper.h
+**/
+
+#pragma once
+
+
+using namespace System;
+
+namespace  NesDbgWrap  {
+namespace  NesMan  {
+
+
+//========================================================================
+//
+//    ReferenceWrapper  class.
+//
+
+template  <typename T>
+public ref  class  ReferenceWrapper
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    ReferenceWrapper();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~ReferenceWrapper();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （ファイナライザ）。
+    **
+    **/
+    !ReferenceWrapper();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+private:
+
+};
+
+}   //  End of namespace  NesMan
+}   //  End of namespace  NesDbgWrap
