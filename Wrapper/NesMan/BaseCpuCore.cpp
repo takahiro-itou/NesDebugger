a287f0bce596bca604ab0b128be3985f42babdd2
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-02-27 20:35:24 +0900

    CPU クラスのラッパークラスを新規作成

---
+++ Wrapper/NesMan/BaseCpuCore.cpp
@@ -0,0 +1,122 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of BaseCpuCore class.
+**
+**      @file       Images/BaseCpuCore.cpp
+**/
+
+#include    "PreCompile.h"
+
+#include    "BaseCpuCore.h"
+
+
+namespace  NesDbgWrap  {
+namespace  NesMan  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace
+
+
+//========================================================================
+//
+//    BaseCpuCore  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （コンストラクタ）。
+//
+
+BaseCpuCore::BaseCpuCore(
+        WrapTarget * const  ptrObj)
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+BaseCpuCore::~BaseCpuCore()
+{
+    //  マネージドリソースを破棄する。              //
+
+    //  続いて、アンマネージドリソースも破棄する。  //
+    this->!BaseCpuCore();
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （ファイナライザ）。
+//
+
+BaseCpuCore::!BaseCpuCore()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  NesMan
+}   //  End of namespace  NesDbgWrap
---
+++ Wrapper/NesMan/BaseCpuCore.h
@@ -0,0 +1,143 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of BaseCpuCore class.
+**
+**      @file       NesMan/BaseCpuCore.h
+**/
+
+#pragma once
+
+#if !defined( NESDBG_NESMAN_INCLUDED_BASE_CPU_CORE_H )
+#    include    "NesDbg/NesMan/BaseCpuCore.h"
+#endif
+
+
+//  クラスの前方宣言。  //
+namespace  NesDbg  {
+namespace  NesMan  {
+class   NesManager;
+class   BaseCpuCore;
+}   //  End of namespace  NesMan
+}   //  End of namespace  NesDbg
+
+using namespace System;
+
+namespace  NesDbgWrap  {
+namespace  NesMan  {
+
+//  クラスの前方宣言。  //
+ref  class  NesManager;
+
+
+//========================================================================
+//
+//    BaseCpuCore  class.
+//
+
+public ref  class  BaseCpuCore
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+private:
+
+    typedef     NesDbg::NesMan::BaseCpuCore     WrapTarget;
+
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （コンストラクタ）。
+    **
+    **/
+    BaseCpuCore(
+            WrapTarget * const  ptrObj);
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~BaseCpuCore();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （ファイナライザ）。
+    **
+    **/
+    !BaseCpuCore();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Properties.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+};
+
+}   //  End of namespace  NesMan
+}   //  End of namespace  NesDbgWrap
