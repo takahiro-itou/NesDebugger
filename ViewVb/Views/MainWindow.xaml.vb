a3b55ef5143bcf2376da7eb625ef4dd7421b5b2f
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-10 19:41:16 +0900

    メインウィンドウのイベントハンドラを実装
    (1) 暫定的に WinForms 版のコードをコピー

--- ViewVb/Views/MainWindow.xaml.vb
+++ ViewVb/Views/MainWindow.xaml.vb
@@ -3,6 +3,115 @@ Namespace Global.NesDebugView.Views
 
 Public Class MainWindow
 
+Private m_manNes As NesDbgWrap.NesMan.NesManager
+Private m_manPpu As NesDbgWrap.NesMan.BasePpuCore
+
+
+Private Function isInitialized() As Boolean
+''--------------------------------------------------------------------
+''    初期化が完了しているか否かを返す。
+''--------------------------------------------------------------------
+    If (Me.m_manPpu Is Nothing) Then Return False
+    Return True
+End Function
+
+Private Function initializeScreen(
+        ByVal w As Integer, ByVal h As Integer) As Boolean
+''--------------------------------------------------------------------
+''    画面を初期化する
+''--------------------------------------------------------------------
+    Me.wfcGameView.initializeScreenImage(w, h)
+    Me.wfcGameView.setupPpuManager(Me.m_manPpu)
+    showGameScreen()
+    Return True
+End Function
+
+Private Function openRomFile(ByVal fileName As String) As Boolean
+''--------------------------------------------------------------------
+''    ゲームをロードする。
+''--------------------------------------------------------------------
+Dim p As System.Drawing.Point
+
+    Me.m_manNes = New NesDbgWrap.NesMan.NesManager
+    Me.m_manNes.openRomFile(fileName)
+
+    Me.m_manPpu = Me.m_manNes.getOrCreatePpuInstance()
+    Me.m_manNes.emulatePowerOn()
+
+    initializeScreen(512, 480)
+
+    Me.Text = "SCAN:" & p.x & "," & p.y & " PC:" &
+            HEX(Me.m_manNes.getNextPC())
+
+    openRomFile = True
+End Function
+
+Private Sub showGameScreen()
+''--------------------------------------------------------------------
+''    画像を表示する
+''--------------------------------------------------------------------
+    If Not isInitialized() Then Exit Sub
+
+    Me.wfcGameView.drawScreen()
+    Me.wfcGameView.showScreen()
+End Sub
+
+Private Sub MainView_Load(sender As Object, e As EventArgs) Handles _
+            MyBase.Load
+''--------------------------------------------------------------------
+''    フォームのロードイベントハンドラ
+''--------------------------------------------------------------------
+
+End Sub
+
+Private Sub mnuFileExit_Click(sender As Object, e As EventArgs)
+''--------------------------------------------------------------------
+''    メニュー「ファイル」－「終了」
+''--------------------------------------------------------------------
+    Application.Exit()
+End Sub
+
+Private Sub mnuFileOpen_Click(sender As Object, e As EventArgs)
+''--------------------------------------------------------------------
+''    メニュー「ファイル」－「開く」
+''--------------------------------------------------------------------
+Dim dlgOpenFile As New Microsoft.Win32.OpenFileDialog
+
+    With dlgOpenFile
+        .DefaultExt = ".nes"
+        .FileName = "*.nes"
+        .Filter = "Nes Image(*.nes)|*.nes|All files(*.*)|*.*"
+        .FilterIndex = 1
+
+        If .ShowDialog() = True Then
+            openRomFile(.FileName)
+        End If
+    End With
+
+End Sub
+
+Private Sub mnuRunCount_Click(sender As Object, e As EventArgs)
+''--------------------------------------------------------------------
+''    メニュー「実行」－「カウント」
+''--------------------------------------------------------------------
+Dim i As Integer
+Dim p As System.Drawing.Point
+
+    For i = 0 To 180
+        Me.m_manNes.executeInstructions(5000, 10000)
+        p = Me.m_manPpu.getCurrentScanPoint()
+
+        showGameScreen()
+        Me.Text = i & " SCAN:" & p.x & "," & p.y & " PC:" &
+                HEX(Me.m_manNes.getNextPC())
+        Application.DoEvents()
+    Next i
+    System.Threading.Thread.Sleep(16)
+    MessageBox.Show("実行完了")
+
+End Sub
+
+
 End Class
 
 End Namespace
